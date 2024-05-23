c238ea09222ac56f265fab401596b3aaa4073ba7
Jairo <jairoregiscavalcanti@gmail.com>
2024-05-23 17:27:12 -0300

    Classe_principal_e_posicao_inseridas

---
+++ Projeto/Principal.java
@@ -0,0 +1,11 @@
+package Projeto;
+
+import Tabuleiro.Posicao;
+
+public class Principal {
+    public static void main(String[] args) {
+       Posicao pos = new Posicao(3,5);
+       System.out.println(pos);
+    }
+
+}
---
+++ Tabuleiro/Posicao.java
@@ -0,0 +1,35 @@
+package Tabuleiro;
+
+public class Posicao {
+
+    private int linha;
+    private int column;
+    
+    public Posicao(int linha, int column) {
+        this.linha = linha;
+        this.column = column;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return linha + "," + column;
+    }
+
+    
+}
